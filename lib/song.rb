432b56f6d4f4ed1862bb13fe7c5088c50a031201
Corey H <c.l.halicke@gmail.com>
2019-06-26 00:40:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,8 +35,6 @@ class Song
 
   def self.find_or_create_by_name(name)
       if self.find_by_name(name)
-        x = self.find_by_name(name)
-        x.name
       else self.create_by_name(name)
       end
   end
